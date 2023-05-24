42605c3cbc36684bc17d1dee1a198919902af9cc
西野充洋 <tnhtx.17.m@gmail.com>
2023-05-24 13:25:08 +0900

    追加 生成した画像をDBに登録するロジック追加

--- app/controllers/api/v1/image_texts_controller.rb
+++ app/controllers/api/v1/image_texts_controller.rb
@@ -2,35 +2,37 @@ class Api::V1::ImageTextsController < ApplicationController
   require 'mini_magick'
 
   def create
-    buybug
     image_text = ImageText.new(answer1: params[:answer1], answer2: params[:answer2], answer3: params[:answer3])
-    render json: { errors: image_text.errors.full_messages }, status: :unprocessable_entity
       
-    # # MiniMagickを使って画像を読み込みます。
-    # image = MiniMagick::Image.open('path_to_your_base_image.jpg')
+    # MiniMagickを使って画像を読み込む。
+    image = MiniMagick::Image.open(Rails.root.join('public', 'images', 'template1.png'))
 
-    # # 画像にテキストを追加します。
-    # image.combine_options do |c|
-    #   c.gravity 'Center'
-    #   c.pointsize '32'
-    #   c.draw "text 0,0 '#{image_text.text}'"
-    #   c.fill 'white'
-    # end
+    # 画像にテキストを追加する。
+    image.combine_options do |c|
+      c.gravity 'North'
+      c.pointsize '32'
+      c.font Rails.root.join('public', 'fonts', 'Yomogi.ttf')  
+      c.draw "text 0,180 '#{image_text.answer1}'"
+      c.draw "text 0,325 '#{image_text.answer2}'"
+      c.draw "text 0,475 '#{image_text.answer3}'"
+      c.fill 'black'
+    end
 
-    # # テキストが追加された画像を一時的なファイルに保存します。
-    # temp_image_path = Rails.root.join('tmp', 'temp_image.jpg')
-    # image.write(temp_image_path)
+    # テキストが追加された画像を一時的なファイルに保存する。
+    temp_image_path = Rails.root.join('tmp', 'temp_image.jpg')
+    image.write(temp_image_path)
 
-    # # 画像をActiveStorageにアップロードします。
-    # image_text.image.attach(io: File.open(temp_image_path), filename: 'temp_image.jpg')
+    # 画像をActiveStorageにアップロードします。
+    image_text.image.attach(io: File.open(temp_image_path), filename: 'temp_image.jpg')
 
-    # # 一時ファイルを削除します。
-    # File.delete(temp_image_path)
+    # 一時ファイルを削除する。
+    File.delete(temp_image_path)
 
-    # if image_text.save
-    #   render json: { url: url_for(image_text.image) }
-    # else
-    #   render json: { errors: image_text.errors.full_messages }, status: :unprocessable_entity
-    # end
+    if image_text.save
+      render json: { url: url_for(image_text.image) }
+    else
+      render json: { errors: image_text.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 end
+
