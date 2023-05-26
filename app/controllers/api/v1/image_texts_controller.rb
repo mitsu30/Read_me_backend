e5dbc8dbae96cd3f2dac580ac3adc7f8a0aad332
西野充洋 <tnhtx.17.m@gmail.com>
2023-05-26 18:18:56 +0900

    修正 画像保存のロジックを修正

--- app/controllers/api/v1/image_texts_controller.rb
+++ app/controllers/api/v1/image_texts_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ImageTextsController < ApplicationController
   require 'mini_magick'
-  require 'base64' # 追加
+  require 'base64' 
 
   def preview
     image_text = ImageText.new(answer1: params[:image_text][:answer1], answer2: params[:image_text][:answer2], answer3: params[:image_text][:answer3])
@@ -9,26 +9,32 @@ class Api::V1::ImageTextsController < ApplicationController
     temp_image_path = Rails.root.join('tmp', 'temp_image.jpg')
     image.write(temp_image_path)
 
-    encoded_image = Base64.encode64(File.open(temp_image_path).read) # 追加
-    File.delete(temp_image_path) # 追加
+    encoded_image = Base64.encode64(File.open(temp_image_path).read)
+    File.delete(temp_image_path)
 
-    render json: { url: "data:image/jpg;base64,#{encoded_image}" } # 変更
+    render json: { url: "data:image/jpg;base64,#{encoded_image}" } 
   end
 
   def create
     image_text = ImageText.new(answer1: params[:image_text][:answer1], answer2: params[:image_text][:answer2], answer3: params[:image_text][:answer3])
-    image = generate_image(image_text)
-
-    temp_image_path = Rails.root.join('tmp', 'temp_image.jpg')
-    image.write(temp_image_path)
-
-    image_text.image.attach(io: File.open(temp_image_path), filename: 'temp_image.jpg')
-
+  
+    # まずは ImageText オブジェクトを保存
     if image_text.save
-      File.delete(temp_image_path) # 追加
-      render json: { url: url_for(image_text.image), id: image_text.id } # Include id in the response
+      begin
+        # 例外が発生する可能性のあるコードを begin-end ブロック内に配置
+        image = generate_image(image_text)
+        temp_image_path = Rails.root.join('tmp', 'temp_image.jpg')
+        image.write(temp_image_path)
+        
+        image_text.image.attach(io: File.open(temp_image_path), filename: 'temp_image.jpg')
+        File.delete(temp_image_path)
+        
+        render json: { url: url_for(image_text.image), id: image_text.id }
+      rescue => e
+        # 何か問題が発生した場合は、そのエラーを捕捉し、エラーメッセージを返す
+        render json: { errors: [e.message] }, status: :internal_server_error
+      end
     else
-      File.delete(temp_image_path) # 追加
       render json: { errors: image_text.errors.full_messages }, status: :unprocessable_entity
     end
   end
