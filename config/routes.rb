4dc7c9ddd965a350142908e937be02fa6b24cff0
西野充洋 <tnhtx.17.m@gmail.com>
2023-05-26 16:12:22 +0900

    追加 showアクションへのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users
       post '/image_texts/preview', to: 'image_texts#preview'
-      post '/image_texts', to: 'image_texts#create'
+      post '/image_texts',         to: 'image_texts#create'
+      get  '/image_texts',         to: 'image_texts#show'
     end
   end
 end
