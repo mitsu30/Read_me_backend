96e950cd3ca9b5ca72301743320ade9d4e7c41d5
西野充洋 <tnhtx.17.m@gmail.com>
2023-05-27 13:49:15 +0900

    修正 ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :image_text, only: [:create, :show]
       post '/image_texts/preview', to: 'image_texts#preview'
-      post '/image_texts',         to: 'image_texts#create'
-      get  '/image_texts',         to: 'image_texts#show'
     end
   end
 end
