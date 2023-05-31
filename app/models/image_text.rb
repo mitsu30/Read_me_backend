a2d292247ac595c3134fd358301ba3de18b3b60e
西野充洋 <tnhtx.17.m@gmail.com>
2023-05-31 11:24:41 +0900

    修正 文字数バリデーション変更

--- app/models/image_text.rb
+++ app/models/image_text.rb
@@ -1,8 +1,8 @@
 class ImageText < ApplicationRecord
   has_one_attached :image
 
-  validates :answer1, length: { maximum: 32 }
-  validates :answer2, length: { maximum: 16 }
+  validates :answer1, length: { maximum: 26 }
+  validates :answer2, length: { maximum: 13 }
   validate :validate_answer3
 
   private
@@ -14,7 +14,7 @@ class ImageText < ApplicationRecord
     if lines.length > 3
       errors.add(:answer3, '3行以内で入力してください。')
     elsif lines.any? { |line| line.length > 32 }
-      errors.add(:answer3, '各行32文字以内で入力してください。')
+      errors.add(:answer3, '各行26文字以内で入力してください。')
     end
   end
 end
