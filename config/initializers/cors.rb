719464212cea3bc789bfe599f3926183e07c09b1
西野充洋 <tnhtx.17.m@gmail.com>
2023-06-01 11:30:24 +0900

    修正 不要ファイル削除

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:3001', 'https://readmeee.vercel.app', 'https://read-me-frontend-git-14mvp3-mitsu30.vercel.app'
-  
 
     resource '*',
       headers: :any,
