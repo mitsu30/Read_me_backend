30abba51de9dd87049c1207dde8b30b9e03f6e4a
西野充洋 <tnhtx.17.m@gmail.com>
2023-06-02 15:24:11 +0900

    追加 Notoフォントファイル

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', 'https://readmeee.vercel.app''
+    origins 'http://localhost:3001', 'https://readmeee.vercel.app'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
