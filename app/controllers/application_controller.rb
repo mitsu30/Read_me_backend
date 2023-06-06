ed05770b4d4f17894060f5bea910f01a9c878948
西野充洋 <tnhtx.17.m@gmail.com>
2023-06-06 11:15:28 +0900

    追加 apllicationコントローラーにログイン処理追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::API
+  include FirebaseAuth
+  include Api::ExceptionHandler
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_action :authenticate_token
+
+  # HTTPトークンによる認証を行う
+  # リクエストヘッダーからトークンを取り出し、FirebaseAuthモジュールのメソッドを使ってトークンを検証する。
+  def authenticate_token
+    authenticate_with_http_token do |token, _options|
+      result = verify_id_token(token)
+
+      if result[:errors]
+        render_400(nil, result[:errors])
+      else
+        # 条件に合うユーザーを検索し@_current_userに代入、いなければUserを作成した上で代入
+        @_current_user = User.find_or_create_by!(uid: result[:uid])
+      end
+    end
+  end
+
+  def current_user
+    @_current_user
+  end
 end
