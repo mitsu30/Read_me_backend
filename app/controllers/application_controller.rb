6be14caa7029daa3a9ee366b49063dfff7d0f730
西野充洋 <tnhtx.17.m@gmail.com>
2023-06-07 18:23:39 +0900

    追加 GitHub APIからRUNTEQのユーザーデータ取得ずみ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,14 +12,34 @@ class ApplicationController < ActionController::API
   def authenticate_token
     authenticate_with_http_token do |token, _options|
       result = verify_id_token(token)
-      byebug
       if result[:errors]
         render_400(nil, result[:errors])
       else
-
-        # 条件に合うユーザーを検索し@_current_userに代入、いなければUserを作成した上で代入
-        @_current_user = User.find_or_create_by!(uid: result[:uid])
+        # ユーザーのemailが組織のメンバーのものであるか確認
+        if member_email_of_organization?(result[:email], 'runteq')
+          @_current_user = User.find_or_create_by!(uid: result[:uid])
+        else
+          render_400(nil, 'User email is not found in the specified organization.')
+        end
       end
+    end
+  end
+
+  # 特定の組織のメンバー情報を取得する
+  def organization_members(organization_name)
+    client = Octokit::Client.new(access_token: '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')
+    client.auto_paginate = true
+    client.organization_members(organization_name).map(&:login)
+  end
+
+  # 指定したユーザーのemailが組織のメンバーのものであるか確認する
+  def member_email_of_organization?(email, organization_name)
+    members = organization_members(organization_name)
+    client = Octokit::Client.new(access_token: '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')
+    byebug
+    members.any? do |member|
+      user = client.user(member)
+      user.email == email
     end
   end
 
