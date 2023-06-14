c4eeb75ba25febc5df813fd2ba6144e0bce7250b
西野充洋 <tnhtx.17.m@gmail.com>
2023-06-14 10:54:29 +0900

    追加 group,user_groupモデルアソシエーション、バリデーション追加

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,6 +1,7 @@
 class Community < ApplicationRecord
   belongs_to :user
   has_many :user_communities, dependent: :destroy
+  has_many :groups, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 255 }
   validates :description, presence: true, length: { maximum: 255 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :user_communities, dependent: :destroy
   has_many :membered_communities, through: :user_communities, source: :community  # ユーザーが所属しているコミュニティ
   
+  has_many :user_groups, dependent: :destroy
+  has_many :membered_groups, through: :user_groups, source: :group  # ユーザーが所属しているグループ
+  
   validates :name, presence: true, length: { maximum: 255 }
   validates :uid, presence: true, uniqueness: true
   validates :role, presence: true
