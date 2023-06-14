526731bbcbcdd1aea9232047df1bcf68ffe1b9f7
西野充洋 <tnhtx.17.m@gmail.com>
2023-06-14 10:30:26 +0900

    追加 user,community, user_communityモデルのアソシエーション、バリデーション設定

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,3 +1,8 @@
 class Community < ApplicationRecord
   belongs_to :user
+  has_many :user_communities, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :description, presence: true, length: { maximum: 255 }
+  validates :password, presence: true, length: { minimum: 3, maximum: 255 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :communities, dependent: :destroy # ユーザーが作成したコミュニティ
+  has_many :user_communities, dependent: :destroy
+  has_many :membered_communities, through: :user_communities, source: :community  # ユーザーが所属しているコミュニティ
+  
   validates :name, presence: true, length: { maximum: 255 }
   validates :uid, presence: true, uniqueness: true
   validates :role, presence: true
