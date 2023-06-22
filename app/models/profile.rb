a1ef62795f28858d3f30d326ff0d6ea42b670d7d
西野充洋 <tnhtx.17.m@gmail.com>
2023-06-22 09:43:02 +0900

    追加 answerモデル、マイグレーション追加

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,7 @@
 class Profile < ApplicationRecord
   belongs_to :user
   has_many :open_ranges, dependent: :destroy
+  has_many :answers, dependent: :destroy
 
   validates :image_url, presence: true, length: { maximum: 255 }
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :template
+  has_many :answers, dependent: :destroy
 
   validates :item,  numericality: { only_integer: true }
 end
