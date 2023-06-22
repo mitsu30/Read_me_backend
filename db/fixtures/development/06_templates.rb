7501c0556e44c367d4c4a8aec25580b8859aec7e
西野充洋 <tnhtx.17.m@gmail.com>
2023-06-22 10:21:35 +0900

    追加 templeteのseedファイル追加

---
+++ db/fixtures/development/06_templates.rb
@@ -0,0 +1,4 @@
+Template.seed(:id,
+  { id: 1, image_url: 'http://example.com/image1.png', next_url: '/next/page/1' },
+  { id: 2, image_url: 'http://example.com/image2.png', next_url: '/next/page/2' },
+)
