30fd77b12de8c52a4da6cabe70327604c92c6dda
西野充洋 <tnhtx.17.m@gmail.com>
2023-06-25 14:17:08 +0900

    修正 user作成時フロントに返すjsonにstatus追加

--- app/controllers/api/v1/authentications_controller.rb
+++ app/controllers/api/v1/authentications_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::AuthenticationsController < ApplicationController
   def create
-    render json: { message: "User successfully logged in!" } if current_user
+    render json: {  status: 'success', message: "User successfully logged in!" } if current_user
   end
 end
 
