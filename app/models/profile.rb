8739acd4fc4d5c74f80e1de2b4756c0370becf47
西野充洋 <tnhtx.17.m@gmail.com>
2023-06-26 13:26:47 +0900

    追加 profileモデルprivacyカラムにカスタムバリデータ適用

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -8,6 +8,15 @@ class Profile < ApplicationRecord
 
   validates :uuid, presence: true, length: { maximum: 255 }
   validates :privacy, presence: true
+  validate :ensure_open_range_present_if_membered_communities_only, if: -> { membered_communities_only? }
 
   enum privacy: { opened: 0, closed: 1, membered_communities_only: 2}
+
+  private
+
+  def ensure_open_range_present_if_membered_communities_only
+    if open_ranges.empty?
+      errors.add(:base, "プロフィール帳を公開するコミュニティを選択してください")
+    end
+  end
 end
