a945b24a5bb5685d38db770c66208d192e30b61a
西野充洋 <tnhtx.17.m@gmail.com>
2023-06-26 13:14:30 +0900

    追加 open_rangesコントローラー複数コミュニティに対応

--- app/controllers/api/v1/open_ranges_controller.rb
+++ app/controllers/api/v1/open_ranges_controller.rb
@@ -1,9 +1,16 @@
 class Api::V1::OpenRangesController < ApplicationController
-  before_action :set_user, :set_profile, :set_community, only: :update
+  before_action :set_user, :set_profile, only: :update
+  before_action :set_communities, only: :update, if: :membered_communities_only?
 
   def update
     @profile.update!(privacy: profile_params[:privacy])
-    @profile.open_ranges.create!(community: @community)
+    if membered_communities_only?
+      @communities.each do |community|
+        unless @profile.open_ranges.exists?(community: community)
+          @profile.open_ranges.create!(community: community)
+        end
+      end
+    end
   end
 
   private
@@ -16,11 +23,15 @@ class Api::V1::OpenRangesController < ApplicationController
     @profile = @user.profiles.find_by(uuid: params[:id])
   end
 
-  def set_community
-    @community = @user.membered_communities.find(params[:community_id])
+  def set_communities
+    @communities = @user.membered_communities.where(id: params[:community_id]) if params[:community_id].present?
   end
 
   def profile_params
     params.require(:profile).permit(:privacy)
+  end
+
+  def membered_communities_only?
+    params.dig(:profile, :privacy) == 'membered_communities_only'
   end
 end
