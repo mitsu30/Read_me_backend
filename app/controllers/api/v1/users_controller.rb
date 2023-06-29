cfe0def1cb4baa7fc4e7e94897f8319e80f3815e
西野充洋 <tnhtx.17.m@gmail.com>
2023-06-29 17:45:05 +0900

    修正 usersコントローラーリファクタリング

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,8 +2,6 @@ class Api::V1::UsersController < ApplicationController
   skip_before_action :authenticate_token, only: [:index, :show_public]
 
   def index
-    # users = User.includes(:user_groups, :membered_groups, :user_communities)
-    #             .where(user_communities: { community_id: 1 })
     users = User.joins(:user_groups, :membered_groups, :user_communities)
                 .where(user_communities: { community_id: 1 },
                        membered_groups: { community_id: 1 })
@@ -28,22 +26,18 @@ class Api::V1::UsersController < ApplicationController
     @user = current_user
     @showed_user = User.find(params[:id])
     if @showed_user
-      showed_user_data = build_user_data
-      showed_user_data[:profiles] = build_profiles_data
-      render json: { status: 'SUCCESS', message: 'Loaded the user', data: showed_user_data }
+      render_user_data(method(:build_profiles_data))
     else
-      render json: { status: 'ERROR', message: 'User not found' }
+      render_user_not_found
     end
   end
 
   def show_public
     @showed_user = User.find(params[:id])
     if @showed_user
-      showed_user_data = build_user_data
-      showed_user_data[:profiles] = build_profiles_data_public
-      render json: { status: 'SUCCESS', message: 'Loaded the user', data: showed_user_data }
+      render_user_data(method(:build_profiles_data_public))
     else
-      render json: { status: 'ERROR', message: 'User not found' }
+      render_user_not_found
     end
   end
 
@@ -81,7 +75,6 @@ class Api::V1::UsersController < ApplicationController
   def build_profiles_data
     user_communities = @user.membered_communities.map(&:id) 
     @showed_user.profiles.with_attached_image.select do |profile|
-      # profileが全体公開か、特定のコミュニティ公開でcurrent_userがそのコミュニティに所属している場合のみtrueを返す
       profile.privacy == 'opened' || (profile.privacy == 'membered_communities_only' && (user_communities & profile.open_ranges.map(&:community_id)).any?)
     end.map do |p| 
       {
@@ -103,5 +96,14 @@ class Api::V1::UsersController < ApplicationController
       }
     end
   end
-end
+  
+  def render_user_data(profile_builder)
+    showed_user_data = build_user_data
+    showed_user_data[:profiles] = profile_builder.call
+    render json: { status: 'SUCCESS', message: 'Loaded the user', data: showed_user_data }
+  end
 
+  def render_user_not_found
+    render json: { status: 'ERROR', message: 'User not found' }
+  end
+end
