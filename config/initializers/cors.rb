c193ec5427b1aff2de422e7445e9223e230c02b8
西野充洋 <tnhtx.17.m@gmail.com>
2023-06-30 22:01:54 +0900

    追加 本番環境用のseedファイル追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', 'https://readmeee.vercel.app'
+    origins 'http://localhost:3001', 'https://readmeee.vercel.app','https://read-me-frontend-git-19crud-mitsu30.vercel.app'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
--- db/fixtures/production/02_communities.rb
+++ db/fixtures/production/02_communities.rb
@@ -1,7 +1,7 @@
 Community.seed do |s|
   s.id = 1
-  s.name = "RUNTEQ"
-  s.description = "RUNTEQのコミュニティ"
+  s.name = "スクール"
+  s.description = "スクールのコミュニティ"
   s.password = "password"
   s.owner_id = "1"
 end
