7ac2ca8a7abedc8920a27759c89568eb566fe79c
西野充洋 <tnhtx.17.m@gmail.com>
2023-07-01 14:57:59 +0900

    追加 twitter_shareコントローラーIDトークン検証スキップ

--- app/controllers/api/v1/twitter_share_controller.rb
+++ app/controllers/api/v1/twitter_share_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::TwitterShareController < ApplicationController
+  skip_before_action :authenticate_token
+  
   def show
     profile = Profile.find_by(uuid: params[:id])
     if profile
