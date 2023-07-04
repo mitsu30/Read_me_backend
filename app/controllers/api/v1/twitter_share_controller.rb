afe4af979ccb7c9f869d6880feced94f45ba568c
西野充洋 <tnhtx.17.m@gmail.com>
2023-07-04 15:52:48 +0900

    追加 Twitterロボ用コントローラー変更

--- app/controllers/api/v1/twitter_share_controller.rb
+++ app/controllers/api/v1/twitter_share_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::TwitterShareController < ApplicationController
   def show
     profile = Profile.find_by(uuid: params[:id])
     if profile
-      render json: { image_url: profile.image.url }
+      render json: { image_url: profile.image.url, privacy: profile.privacy }
     else
       render json: { error: "Image not found" }, status: :not_found
     end
