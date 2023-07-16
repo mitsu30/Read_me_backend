7d8fe2605cf8d8dcf6ae5bb46d40db85b8fb923f
西野充洋 <tnhtx.17.m@gmail.com>
2023-07-16 15:58:03 +0900

    add_likes_controller_createアクション、destroyアクション

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -1,6 +1,25 @@
 class Api::V1::LikesController < ApplicationController
   def create
     user = current_user
+    profile = Profile.find_by(uuid: params[:id])
+
+    begin
+      user.like(profile)
+      return { status: 'success', message: 'Like created successfully.'}
+    rescue => e
+      Rails.logger.error "Like creation failed: #{e.message}"
+      return { status: 'error', message: 'Like creation failed: ' + e.message }, status: :internal_server_error
+  end
+
+  def destroy
+    user = current_user
+    profile = user.likes.find_by(uuid: params[:id]).profile
+    begin
+      user.unlike(profile)
+      return { status: 'success', message: 'Like destroyed successfully.'}
+    rescue => e
+      Rails.logger.error "Like creation failed: #{e.message}"
+      return { status: 'error', message: 'Like destroy failed: ' + e.message }
   end
 end
 
