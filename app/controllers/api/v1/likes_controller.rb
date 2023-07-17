68b0088571074f491916e273e51015b150b803e9
西野充洋 <tnhtx.17.m@gmail.com>
2023-07-17 16:15:20 +0900

    add_like_controller_destroyアクション

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -4,10 +4,10 @@ class Api::V1::LikesController < ApplicationController
       user = current_user
       profile = Profile.find_by(uuid: params[:profile_id])
       user.like(profile)
-      return { status: 'success', message: 'Like created successfully.'}
+      render json: { status: 'success', message: 'Like created successfully.'}
     rescue => e
       Rails.logger.error "Like creation failed: #{e.message}"
-      return { status: 'error', message: 'Like creation failed: ' + e.message }, status: :internal_server_error
+      render json: { status: 'error', message: 'Like creation failed: ' + e.message }, status: :internal_server_error
     end
   end
 
@@ -21,13 +21,13 @@ class Api::V1::LikesController < ApplicationController
 
   def destroy
     user = current_user
-    profile = user.likes.find_by(uuid: params[:id]).profile
+    profile = user.like_profiles.find_by(uuid: params[:id])
     begin
       user.unlike(profile)
-      return { status: 'success', message: 'Like destroyed successfully.'}
+      render json: { status: 'success', message: 'Like destroyed successfully.'}
     rescue => e
       Rails.logger.error "Like creation failed: #{e.message}"
-      return { status: 'error', message: 'Like destroy failed: ' + e.message }
+      render json: { status: 'error', message: 'Like destroy failed: ' + e.message }
     end
   end
 end
