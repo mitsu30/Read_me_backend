3471b2277365563e686730fc35084ab6a5470dc9
西野充洋 <tnhtx.17.m@gmail.com>
2023-08-02 12:34:37 +0900

    add_seedファイル

--- db/fixtures/development/04_user_communities.rb
+++ db/fixtures/development/04_user_communities.rb
@@ -3,3 +3,12 @@ UserCommunity.seed do |s|
   s.user_id = "1"
   s.community_id = 1
 end
+
+69.times do |i|
+  UserCommunity.seed do |s|
+    s.id = i + 1
+    s.user_id = i + 1
+    s.community_id = 1
+  end
+end
+
---
+++ db/fixtures/development/08_template_2.rb
@@ -0,0 +1,10 @@
+Template.seed do |s|
+  s.id = 4, 
+  s.name = 'あにばーさりー'
+  s.image_path = '/templates/anniversary.png' 
+  s.next_path = '/profiles/anniversary'
+  s.only_student = true
+end
+
+
+
